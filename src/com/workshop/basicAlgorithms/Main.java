738b622147acdae279368567b83d0cf30a989f65
dimit <dimitar_rradev@abv.bg>
2024-11-01 12:07:05 +0200

    Out of brainpower, seriosly need reset

--- src/com/workshop/basicAlgorithms/Main.java
+++ src/com/workshop/basicAlgorithms/Main.java
@@ -12,11 +12,119 @@ public class Main {
 //        recursiveArraySum();
 //        recursiveFactorial();
 //        sumOfCoins();
-        mergeSort();
+//        sortingAlgorithms();
+        binarySearch();
     }
 
-    private static void mergeSort() {
+    private static void binarySearch() {
+        int[] numbers = getNumbers();
+        int number = Integer.parseInt(SCANNER.nextLine());
 
+        int index = getIndex(numbers, number);
+
+        System.out.println(index);
+    }
+
+    private static int getIndex(int[] arr, int number) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+        int mid = start + (end - start) / 2;
+            if (arr[mid] == number) {
+                return mid;
+            } else if (arr[mid] > number) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+
+    private static void sortingAlgorithms() {
+        int[] numbers = getNumbers();
+//        int[] sorted = mergeSort(numbers);
+
+        sort(numbers);
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+    }
+
+    private static int[] getNumbers() {
+        return Arrays.stream(SCANNER.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static void sort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr, ++i, j);
+            }
+        }
+
+        swap(arr, ++i, high);
+
+        return i;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int middleIndex = arr.length / 2;
+
+        int[] firstPartition = Arrays.copyOfRange(arr, 0, middleIndex);
+        int[] secondPartition = Arrays.copyOfRange(arr, middleIndex, arr.length);
+
+        firstPartition = mergeSort(firstPartition);
+        secondPartition = mergeSort(secondPartition);
+
+        int arrIndex = 0;
+        int firstIndex = 0;
+        int secondIndex = 0;
+
+        while (firstIndex < firstPartition.length && secondIndex < secondPartition.length) {
+            if (firstPartition[firstIndex] < secondPartition[secondIndex]) {
+                arr[arrIndex++] = firstPartition[firstIndex++];
+            } else {
+                arr[arrIndex++] = secondPartition[secondIndex++];
+            }
+        }
+
+        while (firstIndex < firstPartition.length) {
+            arr[arrIndex++] = firstPartition[firstIndex++];
+        }
+
+        while (secondIndex < secondPartition.length) {
+            arr[arrIndex++] = secondPartition[secondIndex++];
+        }
+
+        return arr;
     }
 
     private static void sumOfCoins() {
@@ -76,7 +184,7 @@ public class Main {
     }
 
     private static int[] readNumbersArray() {
-        return Arrays.stream(SCANNER.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        return getNumbers();
     }
 
     private static int getSum(int[] numbers, int index) {
